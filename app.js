e9347a2927c5a6cee7869a45e82468a0de5af418
Vadim Ekler <veklercareer@yandex.ru>
2023-12-13 12:33:39 +0700

    настройка конфигурации

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.listen(PORT);
