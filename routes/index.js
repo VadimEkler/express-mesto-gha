dcdbfcc8fa242190ab8855f7ee9c14f1c3f4021e
Vadim Ekler <veklercareer@yandex.ru>
2024-01-29 01:01:16 +0700

    bugfix: исправить ошибки автотеста

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,14 @@ const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 const NotFoundError = require('../errors/NotFoundError');
 
-router.use('*', (req, res, next) => {
-  next(new NotFoundError('Страница не найдена!'));
-});
-
 router.use('/signup', signupRoutes);
 router.use('/signin', signinRoutes);
 router.use(auth);
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
 
+router.use('*', (req, res, next) => {
+  next(new NotFoundError('Страница не найдена!'));
+});
 
 module.exports = router;
